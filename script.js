7d12845bdcfce0f434dc9eb3ed4b8d8f8d0f61ac
alqusi1992 <35297889+alqusi1992@users.noreply.github.com>
2020-10-01 18:14:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const text = "My Viewrs on Facebook Are The Best";
+let theme = localStorage.getItem("theme");
 
-let index = 0;
-
-function writeText() {
-  document.body.innerText = text.slice(0, index);
-
-  index++;
-
-  if (index > text.length - 1) {
-    index = 0;
-  }
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
 }
 
-setInterval(writeText, 100);
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "default.css";
+  }
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+
+  localStorage.setItem("theme", mode);
+}
